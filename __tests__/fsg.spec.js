57fe3625a698d0177277f2ba02a4fc237fb70fba
manj4ever <dmanjunath@live.com>
2022-12-04 19:51:46 +0530

    chore: cleanup

---
+++ __tests__/fsg.spec.js
@@ -0,0 +1,16 @@
+const { test } = require("tap");
+const fastifySwaggerGenerate = require("..");
+const { getSampleRouteConfigs, getSampleRouteSwaggerDef } = require("./helper");
+
+test("Get Swagger Spec for 2 routes", (t) => {
+  const opts = {};
+  fastifySwaggerGenerate(opts, getSampleRouteConfigs(), (err, results) => {
+    t.notOk(err);
+    t.equal(
+      JSON.stringify(results),
+      JSON.stringify(getSampleRouteSwaggerDef())
+    );
+    t.pass();
+    t.end();
+  });
+});
